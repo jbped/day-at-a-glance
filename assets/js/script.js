2ff0af6ec74633cac6dc3a18d8b018859c0c916e
jbped <pedigojacob@gmail.com>
2021-05-26 21:59:01 -0600

    created page structure, pop hrs, save to localStorage

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+var activities = [];
+var currentTimeObj = moment();
+var hourId = 8;
+
+var bOD = moment("7:00 am", "h:mm a")
+
+$("#currentDay").text(currentTimeObj.format("lll"))
+
+var displayedHour = $(".hour")
+    .val()
+    .trim();
+    console.log(displayedHour);
+
+var checkTime = function() {
+    for(var i = 8; i < 17; i++) {
+        $("hour-id", i)
+    }
+}
+
+$.each($(".row"), function(){
+    $(this).find(".hour").text(bOD.add(1, "h").format("h:mm a"))
+    .attr("hour-id", hourId);
+    $(this).find("#hour-task")
+    .attr("hour-id", hourId);
+    $(this).find(".saveBtn")
+    .attr("hour-id", hourId++);
+})
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var siblingActivity = $(this).sibling("#hour-task")
+    var sibActStr = siblingActivity.val().trim();
+    var sibActHrId = siblingActivity.attr("hour-id")
+
+    activities.push({
+        time: sibActHrId,
+        activity: sibActStr
+    })
+    saveActivities();
+    console.log(activities);
+})
+
+var saveActivities = function() {
+    localStorage.setItem("activities", JSON.stringify(activities));
+}
+
+var loadActivities = function() {
+    activities = JSON.parse(localStorage.getItem("activities"));
+
+    if(!activities) {
+        activities = [];
+    }
+
+    $.each(activities, function(list, string){
+        string.forEach(function(activity) {
+            
+        })
+    })
+}
+
+
+
+
+
+// $.forEach()
+//     if(moment($(".hour").text().trim(), "h:mm a") < currentTimeObj.format("h:mm a")) {
+//         $("#hour-task").addClass(".past");
+//     }
+//     // s
+//     console.log(moment($(".hour").text().trim(), "h:mm a") < currentTimeObj.format("h:mm a"))
+
+// var topOfHour = function (){
+//     if (currentTimeObj.format("mm") === "55")
+//     console.log("hello")
+// }
+// setInterval(topOfHour(), 1000);
+
