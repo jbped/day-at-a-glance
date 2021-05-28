fed7e2a77f00ce6b2735b9c863ab2547e0b722f5
jbped <pedigojacob@gmail.com>
2021-05-27 23:04:51 -0600

    Corrected localStorage bug, added save icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,9 @@
-var customHour = 8
-
-var currentTimeObj = moment(customHour,"H");
+// Starting hour-id value
 var hourId = 8;
-
+// beginning of day time for render time
 var bOD = moment("7:00 am", "h:mm a")
 
-$("#currentDay").text(currentTimeObj.format("lll"))
-
-var displayedHour = $(".hour")
-    .val()
-    .trim();
-    // console.log(displayedHour);
-
-var checkTime = function() {
-    for(var i = 8; i < 17; i++) {
-        $("hour-id", i)
-    }
-}
-
+// Render time and add hour id to each element in the time row
 $.each($(".row"), function(){
     $(this).find(".hour").text(bOD.add(1, "h").format("h:mm a"))
     .attr("hour-id", hourId);
@@ -27,49 +13,48 @@ $.each($(".row"), function(){
     .attr("hour-id", hourId++);
 })
 
+// On save button press, find the users entered string and the hour id
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
     var siblingActivity = $(this).siblings("#hour-task")
     var sibActStr = siblingActivity.val().trim();
     var sibActHrId = siblingActivity.attr("hour-id")
-    
-    // $.each(activities, function(index, obj){
-    //     $.each(obj, function(attr, value){
-    //         console.log(attr + '==' + value)
-    //     });
-    // });
 
-    // var index = activities.findIndex(p => p.time == sibActHrId);
-
-    
-    var objectToDelete = activities.findIndex(object => object.time == sibActHrId);
-    // console.log(objectToDelete);
-    delete activities[objectToDelete]
-    // let removed = activities.splice(objectToDelete, 1);
-    // console.log(removed);
-    // console.log(activities)
-    
-    activities.push({
+// Create an object with the time (hour id) and activity (entered value) add it to the activities object
+    activities[sibActHrId] = {
         time: sibActHrId,
         activity: sibActStr
-    })
-    
-    
+    }
+    // run save activities
     saveActivities();
-    // console.log(activities, objectToDelete);
 })
 
+// save the activities object
 var saveActivities = function() {
     localStorage.setItem("activities", JSON.stringify(activities));
 }
 
+// get the saved activities object from localStorage
 var loadActivities = function() {
     activities = JSON.parse(localStorage.getItem("activities"));
 
+    // If the activity object doesn't exist create blank with appropriate object keys
     if(!activities) {
-        activities = [];
+        activities = {
+            8: {},
+            9: {},
+            10: {},
+            11: {},
+            12: {},
+            1: {},
+            2: {},
+            3: {},
+            4: {},
+            5: {}
+        };
     }
 
+    // Get the hour id, get the activity value, match hourid with the textarea that has the same id and add text to textarea
     $.each(activities, function(index, value) {
         var hour = value.time;
         var activity = value.activity;
@@ -79,35 +64,20 @@ var loadActivities = function() {
         // console.log(hourMatch)
     });
 }
+
+// Run loadActivities on load
 loadActivities(); 
 
-var checkHour = function() {
-    $.each($(".hour-task"), function() {
-        var hourId = $(this).attr("hour-id");
-        // console.log(hourId)
-        var hourMomObj = moment(hourId, "H");
-        // console.log(hourMomObj)   
-        var hourDiff = hourMomObj.diff(currentTimeObj, "hours")
-        // console.log(hourDiff)
-    
-        if (hourDiff < 0) {
-            $(this).addClass("past");
-        } else if (hourDiff === 0) {
-            $(this).addClass("present");
-            
-        } else if (hourDiff > 0) {
-            $(this).addClass("future");
-        }
-    });
-}
-
+// set interval checks every sec for the displayed current time, generate current time, and update classes for past, current, and future.
 setInterval(function(){
+    $("#currentDay").text(moment().format("lll")); 
+
     $.each($(".hour-task"), function() {
         var hourId = $(this).attr("hour-id");
         // console.log(hourId)
         var hourMomObj = moment(hourId, "H");
         // console.log(hourMomObj)   
-        var hourDiff = hourMomObj.diff(currentTimeObj, "hours")
+        var hourDiff = hourMomObj.diff(moment(), "hours")
         // console.log(hourDiff)
     
         if (hourDiff < 0) {
